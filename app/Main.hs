8507b4c9dfaa5818271464a9f2eeb450f9135cf8
Deepak Jayakumar <djayakumar@guidewire.com>
2016-04-06 18:52:10 -0700

    4/6/2016 - Added basic parsers to Main and created env variable dataset

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
 import Lib
+import Options.Applicative
 
 main :: IO ()
-main = someFunc
+main = exportEnvVars
+
+javaVersion :: Parser String
+javaVersion = strOption . long $ "java"
+
+ideaVersion :: Parser String
+ideaVersion = strOption . long $ "idea"
+
+gwbCommands :: Parser [String]
+gwbCommands = some (strArgument . idm $ "gwbOptions")
+
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Lib
-    ( someFunc
+    ( exportEnvVars
     ) where
 
 import Turtle
 import Data.Map as Map
+import Data.Maybe
+import Prelude hiding (FilePath)
 
-someFunc :: IO ()
-someFunc = echo "Hello World"
+data JavaVersion 
+    = Java7 
+    | Java8
+    deriving (Eq, Ord)
+
+data IdeaVersion
+    = Idea14
+    | Idea15
+    deriving (Eq, Ord)
+
+exportEnvVars :: IO ()
+exportEnvVars = putStrLn . show . fromJust . Map.lookup Java8 $ javaPathMap  
+
+javaPathMap :: Map JavaVersion FilePath
+javaPathMap = Map.fromList 
+    [ (Java7, "/usr/lib/jvm/java-7-oracle")
+    , (Java8, "/usr/lib/jvm/java-8-oracle") ]
+
+ideaPathMap :: Map IdeaVersion FilePath
+ideaPathMap = Map.fromList
+    [ (Idea14, "/opt/idea14/idea-IU-141.3056.4")
+    , (Idea15, "/opt/idea15/idea-IU-143.2332.3")]  
+
